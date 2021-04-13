1a8952afb25a4ac7c0f1a0bfa68d1d576929f542
Sébastien Verbois <sebastien.verbois@imio.be>
2021-04-13 09:20:51 +0200

    Update sv-react-hello-element example

--- sv-react-hello-element/index.jsx
+++ sv-react-hello-element/index.jsx
@@ -1,26 +1,42 @@
 let helloReact = React.createElement('h1', null, "Hello React !!!");
 
 let helloProps = React.createElement('a', {
-    href: 'http://www.unamur.be',
-    target: '_blank',
-    className: 'important',
-    style: {'font-style': 'italic'}
-  }, "Hello props !!!");
+  href: 'http://www.unamur.be',
+  target: '_blank',
+  className: 'important',
+  style: { 'font-style': 'italic' }
+}, "Hello props !!!");
+
+let helloList = React.createElement("ul", null,
+  React.createElement("li", null, "First item"),
+  React.createElement("li", null, "Second item"),
+  React.createElement("li", null, "Third item")
+);
 
 let helloJSX = <h1>Hello JSX</h1>;
 
 let helloJSXwithProperties = (
-  <p className="important" style={{'font-style': 'italic'}}>
+  <p className="important" style={{ 'font-style': 'italic' }}>
     Hello props in JSX
   </p>
+);
+
+let helloJSXList = (
+  <ul>
+    <li>First JSX item</li>
+    <li>Second JSX item</li>
+    <li>Third JSX item</li>
+  </ul>
 );
 
 const page = (
   <section>
     {helloReact}
     {helloProps}
+    {helloList}
     {helloJSX}
     {helloJSXwithProperties}
+    {helloJSXList}
   </section>
 );
 
